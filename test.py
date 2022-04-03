e9ac8f073682ffc1ac72d07fcf9a6f09b171c7b9
Alper Berber <berber@sabanciuniv.edu>
2022-04-03 22:37:37 +0300

    test files

--- test.py
+++ test.py
@@ -2,7 +2,7 @@ from github import Github
 import re
 import base64
 
-g = Github("")
+g = Github("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")
 
 count = 0
 
@@ -11,6 +11,8 @@ topic = ['unity', 'unity3d']
 repo = g.search_repositories("topic:{}".format(topic[0]))
 a = repo.get_page(0)
 
+aregex = '\b(public|private|internal|protected|void)\s*s*\b(async)?\s*\b(static|virtual|abstract|void)?\s*\b(async)?\b(Task)?\s*[a-zA-Z]*(\s[A-Za-z_][A-Za-z_0-9]*\s*)\((([a-zA-Z\[\]\<\>]*\s*[A-Za-z_][A-Za-z_0-9]*\s*)[,]?\s*)+\)'
+regex = '([^{]*)((?:\{[^}]*\}|//.*\r?\n|"[^"]*"|[\S\s])*?\{(?:\{[^}]*\}|//.*\r?\n|"[^"]*"|[\S\s])*?)\}'
 for r in repo:
     contents = r.get_contents("")
     while contents:
@@ -21,7 +23,11 @@ for r in repo:
             if re.match(r'.*\.cs$', file_content.name):
                 count += 1
                 plain = base64.b64decode(file_content.content)
-                rm = re.findall('\b(public|private|internal|protected|void)\s*s*\b(async)?\s*\b(static|virtual|abstract|void)?\s*\b(async)?\b(Task)?\s*[a-zA-Z]*(?<method>\s[A-Za-z_][A-Za-z_0-9]*\s*)\((([a-zA-Z\[\]\<\>]*\s*[A-Za-z_][A-Za-z_0-9]*\s*)[,]?\s*)+\)', plain.decode('utf-8'))
-
-                print(rm)
+                plain = plain.decode('utf-8')
+                for x in re.findall(aregex, plain):
+                    lines = x[0].split('\n')
+                    for line in lines:
+                        if '{' not in line and '}' not in line:
+                            print(line.strip())
+                print('\n\n\n\n\n')
                 
