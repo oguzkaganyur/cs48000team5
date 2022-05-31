658cba7585e8212d5bd81edcb039f1ee4a6ea424
Baris Kerem Kurt <kkurt@sabanciuniv.edu>
2022-05-31 22:35:57 +0300

    react + tailwindcss setup

---
+++ web-app/unityscriptsearcher/src/App.js
@@ -0,0 +1,20 @@
+import React, {useState} from "react";
+
+import {Navbar} from "./components/Navbar";
+import {Footer} from "./components/Footer";
+import {Routes} from "./components/Routes";
+
+const App = () => {
+
+    const [darkTheme, setDarkTheme] = useState(false);
+
+    return (
+        <div className={darkTheme ? 'dark' : ''}>
+            <div className="bg-gray-100">
+                App
+            </div>
+        </div>
+    );
+}
+
+export default App;
