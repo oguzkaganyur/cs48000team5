cc3db90235f6035ef206454d2728ac55a312ff97
B. Kerem KURT <kkurt@sabanciuniv.edu>
2022-06-02 22:54:18 +0300

    elasticsearch db connection

--- web-app/unityscriptsearcher/src/components/Results.jsx
+++ web-app/unityscriptsearcher/src/components/Results.jsx
@@ -18,6 +18,60 @@ export const Results = () => {
     }, [searchTerm, location.pathname]);
 
     console.log(location.pathname);
+
+    console.log("results", results);
+    if(loading) return <Loading />;
+
+    switch (location.pathname) {
+
+        case '/search':
+            return (
+                <div className="sm:px-56 flex flex-wrap justify-between space-y-6">
+                    {results?.results?.map(({ _source }, index) => (
+                        <div key={index} className="md:w-2/5 w-full">
+                            <a href={_source.function_url} target="_blank" rel="noreferrer">
+                                <p className="text-sm">{_source.function_url.length > 60 ? _source.function_url.substring(0, 60) : _source.function_url}</p>
+                                <p className="text-lg hover:underline dark:text-blue-300 text-blue-700  ">{_source.function_name}</p>
+                            </a>
+                        </div>
+                    ))}
+                </div>
+            );
+        default:
+            return 'Error...';
+    }
+}
+
+
+
+
+
+
+
+
+
+
+/*
+import React, {useEffect} from "react";
+import {useLocation} from "react-router-dom";
+import {useResultContext} from "../contexts/ResultContextProvider";
+import {Loading} from "./Loading";
+
+export const Results = () => {
+    const {results, loading, getResults, searchTerm} = useResultContext();
+    const location = useLocation();
+
+    useEffect(() => {
+        if (searchTerm !== '') {
+            if (location.pathname === '/videos') {
+                getResults(`/search/q=${searchTerm} videos`);
+            } else {
+                getResults(`${location.pathname}/q=${searchTerm}&num=40`);
+            }
+        }
+    }, [searchTerm, location.pathname]);
+
+    console.log(location.pathname);
     if(loading) return <Loading />;
 
     switch (location.pathname) {
@@ -38,3 +92,4 @@ export const Results = () => {
             return 'Error...';
     }
 }
+ */
--- web-app/unityscriptsearcher/src/contexts/ResultContextProvider.js
+++ web-app/unityscriptsearcher/src/contexts/ResultContextProvider.js
@@ -1,4 +1,78 @@
 import React, { createContext, useContext, useState } from 'react';
+import axios from "axios";
+
+const ResultContext = createContext();
+const baseUrl = 'https://google-search3.p.rapidapi.com/api/v1';
+
+export const ResultContextProvider = ({ children }) => {
+    const [results, setResults] = useState([]);
+    const [loading, setLoading] = useState(false);
+    const [searchTerm, setSearchTerm] = useState('');
+
+    const getResults = async (url) => {
+        setLoading(true);
+
+        console.log("searchTerm: ", searchTerm);
+
+
+        const res = axios.post('http://localhost:5005/search', {
+            query: searchTerm,
+        })
+            .then(function (response) {
+                console.log("axios response: ", response);
+                setResults(response.data);
+                setLoading(false);
+
+                console.log("res: ", res)
+                console.log("response.data: ", response.data)
+            })
+            .catch(function (error) {
+                console.log("axios error: ",error);
+            });
+
+        /*
+
+                const res = await fetch(`${baseUrl}${url}`, {
+                    method: 'GET',
+                    headers: {
+                        'X-User-Agent': 'desktop',
+                        'X-Proxy-Location': 'EU',
+                        'X-RapidAPI-Host': 'google-search3.p.rapidapi.com',
+                        'X-RapidAPI-Key': 'fb5c133520mshfbe823e2a2b412cp192492jsnb0188ada6573'
+                    },
+                });
+         */
+
+        //const data = await res.json();
+
+        //console.log("res: ", res)
+        //console.log("data: ", data)
+
+        //setResults(data);
+        //setLoading(false);
+    };
+
+    return (
+        <ResultContext.Provider value={{ getResults, results, searchTerm, setSearchTerm, loading }}>
+            {children}
+        </ResultContext.Provider>
+    );
+};
+
+export const useResultContext = () => useContext(ResultContext);
+
+
+
+
+
+
+
+
+
+
+
+/*
+import React, { createContext, useContext, useState } from 'react';
 
 const ResultContext = createContext();
 const baseUrl = 'https://google-search3.p.rapidapi.com/api/v1';
@@ -35,3 +109,4 @@ export const ResultContextProvider = ({ children }) => {
 };
 
 export const useResultContext = () => useContext(ResultContext);
+ */
